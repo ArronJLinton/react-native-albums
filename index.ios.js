48e989dcf0594a57028056ecf7f7b618861a10f9
Arron Linton <arronlinton@Arrons-MacBook-Pro.local>
2017-08-13 20:43:42 -0400

    inital commit

---
+++ index.ios.js
@@ -0,0 +1,29 @@
+// code for ios application 
+
+// import a library to help create a component
+// import - es6 method to import libraries 
+import React from 'react';
+// import destructoring
+import { AppRegistry } from 'react-native';
+// import components
+// ./ start in the current directory(albums directory in this case)
+// if its a js file, we do not have to put .js at the end
+import Header from './src/components/header';
+
+
+// create a componenet
+// component is a javaScript function that returns some amount of JSX
+// JSX is a dialect of javascript that tells react native what content to render on the screen
+const App = () => (
+  // component nesting
+  // passing 'props'
+  // pass in a property called headerText
+    <Header headerText={'Albums'} />
+  );
+
+
+// render it to the device
+// in any react-native application, we need to register at least one component
+// only the root component uses 'appRegistry'
+// we pass a function to return the first component to render to our application
+AppRegistry.registerComponent('albums', () => App);
